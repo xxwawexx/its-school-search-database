4e1c03f1f6a97fcce5c8e224fddb2a5441dde7fe
Reyver Cris Laureto <reyver.laureto@gmail.com>
2024-01-22 13:41:03 +0800

    added custom 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Not Found</title>
+</head>
+<body>
+    <h1>Page Not Found (404)</h1>
+    <p>Trying to access: {{ request()->url() }}</p>
+</body>
+</html>
